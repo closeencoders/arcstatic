e0c1c355d35866d7dbd422f0c4ee50decd8e3f88
closeencoders <closeencoders@gmail.com>
2026-07-28 14:00:14 -0500

    fix for absolute path evaluation on asset copy

--- generate/generator.go
+++ generate/generator.go
@@ -82,16 +82,32 @@ func (g *generator) Generate(metadata *source.SiteMetadata) error {
 	if g.ctx.MakeSitemapXML && len(metadata.SiteMapUrlMetadata) > 0 {
 		g.createSitemapFile(baseOutPath, metadata.SiteMapUrlMetadata)
 	}
-	if g.ctx.SiteOutputRoot != "" && g.ctx.SiteOutputRoot != g.ctx.SiteRoot {
+	if !isSameLoc(g.ctx.SiteOutputRoot, g.ctx.SiteRoot) {
 		g.copyAssets(baseOutPath)
 	}
 
 	return nil
 }
 
+func isSameLoc(one string, two string) bool {
+	// attempt to avoid deep eval
+	if one != "" && two != "" && one == two {
+		return true
+	}
+	oneAbs, err := filepath.Abs(one)
+	if err != nil {
+		return false
+	}
+	twoAbs, err := filepath.Abs(two)
+	if err != nil {
+		return false
+	}
+	return oneAbs == twoAbs
+}
+
 func (g *generator) copyAssets(basePath string) error {
 
-	slog.Debug("copying assets", "path", basePath)
+	slog.Debug("copying assets", "path", basePath, "out", g.ctx.SiteOutputRoot)
 	entries, err := os.ReadDir(g.ctx.SiteRoot)
 	if err != nil {
 		return err
